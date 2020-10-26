22332a32d2bcbcd7b3a700aa5b09e76f9da5bca5
christinezosche <christine.zosche@gmail.com>
2020-10-26 09:48:58 -0700

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
+function greet(time) {
+  let hour = time.split(":")[0];
+  const parsed = parseInt(hour);
+  if ( parsed < 12 ) { return `Good Morning`; }
+  if ( parsed >= 12 ) { 
+    if ( parsed < 17 ) { return `Good Afternoon`; }
+    if ( parsed >= 17 ) { return `Good Evening`; }
+  }
+}
+
+function displayMessage(argument) {
+  let content = document.querySelector("h1#greeting");
+  content.innerText = argument
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
